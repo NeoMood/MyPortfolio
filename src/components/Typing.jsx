f7b263f4fe5519177934276a16a7f40c971478c3
NeoMood <saad02.gmira2002@gmail.com>
2024-01-27 16:43:37 +0100

    changin avatar2

--- src/components/Typing.jsx
+++ src/components/Typing.jsx
@@ -14,10 +14,15 @@ export function Typing(props) {
   }, []);
   return (
     <group {...props} ref={group} dispose={null}>
-      <group scale={1.24} position={[1.2,-0.96,0.5]} rotation={[Math.PI/2, Math.PI, Math.PI/2]}>
+      <group
+        scale={1.24}
+        position={[1.2, -0.96, 0.5]}
+        rotation={[Math.PI / 2, Math.PI, Math.PI / 2]}
+      >
         <primitive object={nodes.Hips} />
         <skinnedMesh
           name="EyeLeft"
+          frustumCulled={false}
           geometry={nodes.EyeLeft.geometry}
           material={materials.Wolf3D_Eye}
           skeleton={nodes.EyeLeft.skeleton}
@@ -26,6 +31,7 @@ export function Typing(props) {
         />
         <skinnedMesh
           name="EyeRight"
+          frustumCulled={false}
           geometry={nodes.EyeRight.geometry}
           material={materials.Wolf3D_Eye}
           skeleton={nodes.EyeRight.skeleton}
@@ -34,6 +40,7 @@ export function Typing(props) {
         />
         <skinnedMesh
           name="Wolf3D_Head"
+          frustumCulled={false}
           geometry={nodes.Wolf3D_Head.geometry}
           material={materials.Wolf3D_Skin}
           skeleton={nodes.Wolf3D_Head.skeleton}
@@ -42,6 +49,7 @@ export function Typing(props) {
         />
         <skinnedMesh
           name="Wolf3D_Teeth"
+          frustumCulled={false}
           geometry={nodes.Wolf3D_Teeth.geometry}
           material={materials.Wolf3D_Teeth}
           skeleton={nodes.Wolf3D_Teeth.skeleton}
@@ -49,31 +57,37 @@ export function Typing(props) {
           morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences}
         />
         <skinnedMesh
+          frustumCulled={false}
           geometry={nodes.Wolf3D_Hair.geometry}
           material={materials.Wolf3D_Hair}
           skeleton={nodes.Wolf3D_Hair.skeleton}
         />
         <skinnedMesh
+          frustumCulled={false}
           geometry={nodes.Wolf3D_Glasses.geometry}
           material={materials.Wolf3D_Glasses}
           skeleton={nodes.Wolf3D_Glasses.skeleton}
         />
         <skinnedMesh
+          frustumCulled={false}
           geometry={nodes.Wolf3D_Body.geometry}
           material={materials.Wolf3D_Body}
           skeleton={nodes.Wolf3D_Body.skeleton}
         />
         <skinnedMesh
+          frustumCulled={false}
           geometry={nodes.Wolf3D_Outfit_Bottom.geometry}
           material={materials.Wolf3D_Outfit_Bottom}
           skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton}
         />
         <skinnedMesh
+          frustumCulled={false}
           geometry={nodes.Wolf3D_Outfit_Footwear.geometry}
           material={materials.Wolf3D_Outfit_Footwear}
           skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton}
         />
         <skinnedMesh
+          frustumCulled={false}
           geometry={nodes.Wolf3D_Outfit_Top.geometry}
           material={materials.Wolf3D_Outfit_Top}
           skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
